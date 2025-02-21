4fc099adea17b64775a939889cfc0454ba30cec3
HARSHITJOSHI1102 <harshitjoshi1102@gmail.com>
2025-02-22 01:14:19 +0530

    New Components

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,27 +1,53 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import User from "./components/User/User";
+import Github, { githubInfoLoader } from "./components/Github/Github";
+// import { githubInfoLoader } from "../../loaders/githubLoader";
+// import { githubInfoLoader } from "../../loaders/githubLoader";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+// // const router = createBrowserRouter([
+// //   {
+// //     path: "/",
+// //     element: <Layout />,
+// //     children: [
+// //       {
+// //         path: "",
+// //         element: <Home />,
+// //       },
+// //       {
+// //         path: "about",
+// //         element: <About />,
+// //       },
+// //       {
+// //         path: "contact",
+// //         element: <Contact />,
+// //       },
+//     ],
+//   },
+// ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      {/* <Route path="github" element={<Github />} /> */}
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+    </Route>
+  )
+);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
