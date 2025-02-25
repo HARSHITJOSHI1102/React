a9e16492a0acd7500e526eaf0fc1015638ee7530
HARSHITJOSHI1102 <harshitjoshi1102@gmail.com>
2025-02-25 19:16:30 +0530

    ThemeChanger

---
+++ 09Themeswitcher/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// For adding this functionality we need to add darkMode class this adds this functionality
